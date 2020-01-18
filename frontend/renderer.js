2ad7851b432cf621de0b1a6953cd7e24b9e9a934
Adrian Stratulat <adrian.stratulat91@gmail.com>
2020-01-18 16:05:39 +0200

    frontend: refactor, reduce duplicate code

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,74 +1,34 @@
-var data1, data2, data3;
+var data = [];
 
-function refresh1() {
+function refresh(i) {
 	const req = require('electron').remote.require;
 	const { exec } = req('child_process');
 
 	const testerProcess = exec('./tester',  (e, stdout, stderr)=> {
-
 		if (e instanceof Error) {
 			console.error(e);
 			throw e;
 		}
-		data1 = JSON.parse(stdout);
+		data[i] = JSON.parse(stdout);
 		updateSeries();
 	});
 };
 
-function refresh2() {
-	const req = require('electron').remote.require;
-	const { exec } = req('child_process');
-
-	const testerProcess = exec('./tester',  (e, stdout, stderr)=> {
-
-		if (e instanceof Error) {
-			console.error(e);
-			throw e;
-		}
-		data2 = JSON.parse(stdout);
-		updateSeries();
-	});
-};
-
-function refresh3() {
-	const req = require('electron').remote.require;
-	const { exec } = req('child_process');
-
-	const testerProcess = exec('./tester',  (e, stdout, stderr)=> {
-
-		if (e instanceof Error) {
-			console.error(e);
-			throw e;
-		}
-		data3 = JSON.parse(stdout);
-		updateSeries();
-	});
-};
-
-function clear1(){
-	data1 = undefined;
+function clearSeries(i){
+	data[i] = undefined;
 	updateSeries();
 }
 
-function clear2(){
-	data2 = undefined;
-	updateSeries();
-}
-
-function clear3(){
-	data3 = undefined;
-	updateSeries();
-}
 function updateSeries(){
 	var list = [];
-	if (typeof data1 !== 'undefined'){
-		list.push({name: 'series-1', data: data1.data});
+	if (typeof data[1] !== 'undefined'){
+		list.push({name: 'series-1', data: data[1].data});
 	}
-	if (typeof data2 !== 'undefined'){
-		list.push({name: 'series-2', data: data2.data});
+	if (typeof data[2] !== 'undefined'){
+		list.push({name: 'series-2', data: data[2].data});
 	}
-	if (typeof data3 !== 'undefined'){
-		list.push({name: 'series-3', data: data3.data});
+	if (typeof data[3] !== 'undefined'){
+		list.push({name: 'series-3', data: data[3].data});
 	}
 	
 	chart.updateSeries(list);
