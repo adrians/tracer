9144136bcd47dff5556583cab681ba0ee681f189
Adrian Stratulat <adrian.stratulat91@gmail.com>
2021-01-05 21:57:06 +0200

    frontend: fix #20

--- frontend/main.js
+++ frontend/main.js
@@ -13,7 +13,8 @@ function createWindow () {
     height: 680,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   })
 
